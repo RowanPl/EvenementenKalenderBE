486c5d743f7918ab34d0c8d85a7e67cd66a48ebf
RowanPl <rowanplooij@hotmail.com>
2023-04-13 10:12:20 +0200

    Added Authority, AuthorityKey, User, Its the start of authentication. Also added Calender as it would be easier to logical to make multiple types of calender

--- src/main/java/com/example/evenementenkalenderbe/Model/Evenement.java
+++ src/main/java/com/example/evenementenkalenderbe/Model/Evenement.java
@@ -1,6 +1,8 @@
 package com.example.evenementenkalenderbe.Model;
 
 import jakarta.persistence.*;
+import org.jetbrains.annotations.NotNull;
+
 
 import java.sql.Time;
 import java.util.Date;
@@ -13,15 +15,107 @@ public class Evenement {
     @GeneratedValue
     private Long Id;
 
+    @NotNull
     private String nameOfEvent;
+    @NotNull
     private Date date;
+    @NotNull
     private String linkToEvent;
+    @NotNull
     private String eventType;
+    @NotNull
     private Time time;
+    @NotNull
     private String moreInformation;
+    @NotNull
     private String fileUpload;
+    @NotNull
     private String location;
 
     @ManyToOne
     private EventCreator eventCreator;
+
+    @ManyToOne
+    private Calender calender;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNameOfEvent() {
+        return nameOfEvent;
+    }
+
+    public void setNameOfEvent(String nameOfEvent) {
+        this.nameOfEvent = nameOfEvent;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLinkToEvent() {
+        return linkToEvent;
+    }
+
+    public void setLinkToEvent(String linkToEvent) {
+        this.linkToEvent = linkToEvent;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public String getMoreInformation() {
+        return moreInformation;
+    }
+
+    public void setMoreInformation(String moreInformation) {
+        this.moreInformation = moreInformation;
+    }
+
+    public String getFileUpload() {
+        return fileUpload;
+    }
+
+    public void setFileUpload(String fileUpload) {
+        this.fileUpload = fileUpload;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public EventCreator getEventCreator() {
+        return eventCreator;
+    }
+
+    public void setEventCreator(EventCreator eventCreator) {
+        this.eventCreator = eventCreator;
+    }
 }
--- src/main/java/com/example/evenementenkalenderbe/Model/EventCreator.java
+++ src/main/java/com/example/evenementenkalenderbe/Model/EventCreator.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 @Entity
@@ -13,8 +15,33 @@ public class EventCreator {
     @GeneratedValue
     private long id;
 
+    @NotNull
     private String creatorName;
 
     @OneToMany(mappedBy = "eventCreator")
     private List<Evenement> events;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+
+    public List<Evenement> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Evenement> events) {
+        this.events = events;
+    }
 }
